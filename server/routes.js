7214e08bb6dfbd85af1e340d8d3e20376997316a
Oluwakemi Fasae <kemifasae@yahoo.co.uk>
2017-10-02 02:06:08 +0100

    set api endpoint to get all recipes

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,15 @@
 
+global.recipes = [
+    {id: 1, name: "Rice"}, {id: 2, name: "Rice"}, {id: 3, name: "Rice"}, {id: 4, name: "Beans"}];
+
 const Route = (app) => {
-    console.log('App running here too');
+    app.get('/api/v1/recipes', (req, res) => {
+        return res.json({
+            status: error,
+            message: global.recipes,
+            
+        });
+    });
 }
     
 export default Route;

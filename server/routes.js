961d51fc758e91190f3387dc51ceba7b5578084a
Oluwakemi Fasae <kemifasae@yahoo.co.uk>
2017-10-17 17:23:45 +0100

    update routes and paths

--- server/routes.js
+++ server/routes.js
@@ -7,19 +7,19 @@ const Route = (app) => {
 // Endpoints for users
   app.post('/api/v1/users/login', UserController.login);
   app.post('/api/v1/users/signUp', UserController.create);
-  app.get('/api/v1/users/userid/recipes', Middleware.authorize, RecipeController.getFavoriteRecipes);
+  app.get('/api/v1/users/userId/recipes', Middleware.authorize, RecipeController.getFavoriteRecipes);
 
   // Endpoints for recipes
-  app.post('/api/v1/recipes', RecipeController.create);
+  app.post('/api/v1/recipes', Middleware.authorize, RecipeController.create);
   app.get('/api/v1/recipes', RecipeController.getAllRecipes);
-  // app.get('/api/v1/recipes/:recipeid', RecipeController.retrieve);
+  app.get('/api/v1/recipes/:recipeId', RecipeController.getRecipe);
 
   // route for put
-  app.put('/api/v1/recipes/:recipeid', Middleware.authorize, RecipeController.updateRecipe);
-  // route for delete 
-  app.delete('/api/v1/recipes/:recipeid', Middleware.authorize, RecipeController.deleteRecipe);
-  // route for review 
-  app.post('/api/v1/recipes/:recipeid/reviews', Middleware.authorize, RecipeController.postReview);
+  app.put('/api/v1/recipes/:recipeId', Middleware.authorize, RecipeController.updateRecipe);
+  // route for delete
+  app.delete('/api/v1/recipes/:recipeId', Middleware.authorize, RecipeController.deleteRecipe);
+  // route for review
+  app.post('/api/v1/recipes/:recipeId/reviews', Middleware.authorize, RecipeController.postReview);
   // route for upvotes
   app.get('/api/v1/recipes?sort=upvotes&order=des', RecipeController.getHighRecipes);
 };
